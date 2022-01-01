9e20da3a645903a180515d665876c8893d8e2486
leonardoigor <igor_mendonca@outlook.com.br>
2022-01-01 17:10:08 -0300

    agemt

--- Dueling Double Deep Q Learning/dueling_ddqn.py
+++ Dueling Double Deep Q Learning/dueling_ddqn.py
@@ -58,4 +58,64 @@ class ReplayBuffer():
 
         states = self.state_memory[batch]
         states_ = self.new_state_memory[batch]
-        actions = self.
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        dones = self.terminal_memory[batch]
+
+        return states, actions, rewards, states_, dones
+
+
+class Agent():
+    def __init__(self, lr, gamma, n_actions, epsilon, batch_size, input_dims, epsilon_dec=1e-3,
+                 eps_end=0.01, mem_size=1000000, fc1_dims=256, fc2_dims=256, replace_target_cnt=500):
+        self.action_space = [i for i in range(n_actions)]
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_dec = epsilon_dec
+        self.eps_end = eps_end
+        self.batch_size = batch_size
+        self.replace = replace_target_cnt
+        self.learn_step_counter = 0
+        self.memory = ReplayBuffer(mem_size, input_dims)
+        self.q_val = DuelingDeepQNetwork(n_actions, fc1_dims, fc2_dims)
+        self.q_next = DuelingDeepQNetwork(n_actions, fc1_dims, fc2_dims)
+
+        self.q_val.compile(optimizer=Adam(lr=lr), loss='mse')
+        self.q_next.compile(optimizer=Adam(lr=lr), loss='mse')
+
+    def store_transition(self, state, action, reward, state_, done):
+        self.memory.store_transition(state, action, reward, state_, done)
+
+    def choose_action(self, observation):
+        if random.random() > self.epsilon:
+            state = tf.convert_to_tensor([observation], dtype=tf.float32)
+            actions = self.q_val.advantage(state)
+            action = tf.argmax(actions).numpy()
+        else:
+            action = random.choice(self.action_space)
+        return action
+
+    def learn(self):
+        if self.memory.men_cntr < self.batch_size:
+            return
+
+        if self.learn_step_counter % self.replace == 0:
+            self.q_next.set_weights(self.q_val.get_weights())
+
+        state, action, reward, state_, done = self.memory.sample_buffer(
+            self.memory.sample_buffer(self.batch_size))
+
+        q_pred = self.q_val(state)
+        q_next = self.q_next(state_)
+        q_target = q_pred.numpy()
+        max_actions = tf.argmax(q_next, axis=1)
+
+        for idx, terminal in enumerate(done):
+
+            q_target[idx, action[idx]] = reward[idx] + \
+                self.gamma * q_target[idx, max_actions[idx]]*(1-int(done[idx]))
+
+        self.q_val.train_on_batch(state, q_target)
+        self.epsilon = self.epsilon - \
+            self.epsilon_dec if self.epsilon > self.eps_end else self.eps_end
+        self.learn_step_counter += 1
